8f0f655fa4d7abe2b959a20ded6a1b38ceed3c3b
Vijay Chavre <chavre.vijay@gmail.com>
2024-07-03 09:21:24 +0530

    Added unit test for form category dropdown

--- src/features/Product/__tests__/ProductCreate.test.tsx
+++ src/features/Product/__tests__/ProductCreate.test.tsx
@@ -1,3 +1,4 @@
+// Import necessary modules and components for testing
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import CreateProductForm from "../ProductCreate";
@@ -6,25 +7,41 @@ import { getCategories } from "@/services/product/CategoryService";
 import { prettyDOM } from "@testing-library/dom";
 import { act } from "react-dom/test-utils";
 
+// Mock the product and category service modules
 jest.mock("../../../services/product/productService");
 jest.mock("../../../services/product/CategoryService");
+
+// Define the test suite for CreateProductForm component
 describe("CreateProductForm", () => {
+  // Clear mocks before each test case
   beforeEach(() => {
     (createProduct as jest.Mock).mockClear();
     (getCategories as jest.Mock).mockClear();
   });
 
+  // Test case to check if form elements are rendered correctly
   it("renders the form elements correctly", () => {
+    // Render the CreateProductForm component
     render(<CreateProductForm />);
-    expect(screen.getByLabelText("Product Name")).toBeInTheDocument();
-    expect(screen.getByTestId("Unit")).toBeInTheDocument();
-    //expect(screen.getByTestId("Category")).toBeInTheDocument();
-    expect(screen.getByLabelText("Price")).toBeInTheDocument();
-    expect(screen.getByLabelText("Description")).toBeInTheDocument();
-    expect(screen.getAllByText("Create Product")[0]).toBeInTheDocument();
+
+    // Store form elements in variables
+    const productNameInput = screen.getByLabelText("Product Name");
+    const unitInput = screen.getByTestId("Unit");
+    const priceInput = screen.getByLabelText("Price");
+    const descriptionInput = screen.getByLabelText("Description");
+    const createProductButton = screen.getAllByText("Create Product")[0];
+
+    // Assert that form elements are in the document
+    expect(productNameInput).toBeInTheDocument();
+    expect(unitInput).toBeInTheDocument();
+    // expect(screen.getByTestId("Category")).toBeInTheDocument(); // Uncomment if needed
+    expect(priceInput).toBeInTheDocument();
+    expect(descriptionInput).toBeInTheDocument();
+    expect(createProductButton).toBeInTheDocument();
   });
 
-  it("submits the form with valid data", async () => {
+  // Test case to check form submission with valid data
+  it("submits the form with invalid data", async () => {
     const mockCreateProduct = createProduct as jest.Mock;
     const mockgetCategories = getCategories as jest.Mock;
     mockCreateProduct.mockResolvedValueOnce({ statusCode: 200 });
@@ -46,7 +63,7 @@ describe("CreateProductForm", () => {
     const { container } = render(<CreateProductForm />);
     fireEvent.change(screen.getByRole("textbox", { name: /name/i }), {
       target: {
-        value: "t",
+        value: "tt",
       },
     });
     fireEvent.change(screen.getByLabelText("Price"), {
@@ -61,6 +78,26 @@ describe("CreateProductForm", () => {
     });
 
     expect(mockgetCategories).toHaveBeenCalledTimes(1);
+    fireEvent.change(
+      screen.getByRole("combobox", {
+        name: /category/i,
+      }),
+      {
+        target: { value: "Category 1" },
+      },
+    );
+
+    const category2Option = screen.getByRole("option", {
+      name: /category 2/i,
+    });
+
+    fireEvent.click(category2Option);
+
+    const comboboxCategory = screen.getByRole("combobox", {
+      name: /category/i,
+    });
+
+    expect(comboboxCategory).toHaveValue("Category 2");
 
     const submitButton = screen.getByTestId("create-product-button");
 
@@ -76,7 +113,5 @@ describe("CreateProductForm", () => {
     // Assert that price error is displayed
     const priceError = await screen.findByText("Invalid");
     expect(priceError).toBeInTheDocument();
-
-    // screen.logTestingPlaygroundURL();
   });
 });
