3e507812cefe9420a87cf4ae0edd2d938641886d
Vijay Chavre <chavre.vijay@gmail.com>
2024-07-25 02:18:16 +0530

    Added storybook for product search dropdown

---
+++ src/app/login/loginForm.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StoryObj, Meta } from "@storybook/react";
+import LoginForm from "./loginForm";
+import { AccountProvider } from "@/context/accountProvider";
+import { StyledEngineProvider } from "@mui/material";
+import ThemeProviderWrapper from "@/components/theme/ThemeProvider";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+
+const meta = {
+  component: LoginForm,
+  title: "Components/LoginForm",
+  decorators: [
+    (Story) => (
+      <AccountProvider>
+        <AppRouterCacheProvider>
+          <StyledEngineProvider injectFirst>
+            <ThemeProviderWrapper>
+              <Story />
+            </ThemeProviderWrapper>
+          </StyledEngineProvider>
+        </AppRouterCacheProvider>
+      </AccountProvider>
+    ),
+  ],
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoginFormDefault: Story = {
+  args: {},
+};
---
+++ src/features/Product/Components/ProductDropdown.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StoryObj, Meta } from "@storybook/react";
+import { useForm } from "react-hook-form";
+import ProductDropdown from "./ProductDropdown";
+
+const WrapperComponent = () => {
+  const { control, setValue, handleSubmit } = useForm();
+  return (
+    <form onSubmit={handleSubmit(console.log)}>
+      {" "}
+      <ProductDropdown
+        control={control}
+        name="product"
+        setValue={setValue}
+      />{" "}
+    </form>
+  );
+};
+
+const meta = {
+  title: "Components/ProductDropdown",
+  component: WrapperComponent,
+} satisfies Meta<typeof WrapperComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ProductDropdownDefault: Story = {
+  args: {},
+};
